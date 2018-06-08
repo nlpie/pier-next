6a115f835a4dede75a35809ddca2ada917950c73
Reed McEwan <rmcewan@umn.edu>
2018-06-08 15:04:37 -0500

    Updates #19

--- grails-app/services/edu/umn/nlpie/pier/elastic/search/ElasticService.groovy
+++ grails-app/services/edu/umn/nlpie/pier/elastic/search/ElasticService.groovy
@@ -20,6 +20,16 @@ class ElasticService {
 		rest.post( scrollUrl ) { json JsonOutput.toJson(sp) }
 	}
 	
+	def cui( cui ) {
+		def rest = new RestBuilder()
+		//TODO externalize rest url
+		def body = [ "query": [ "match": [ "cui": "${cui}" ] ] ]
+		rest.post( "http://localhost:9200/umls_2013/entry/_search" ) { json body }
+		//println r.json.toString(2)
+		//println "--------------------"
+
+	}
+	
 	def fetchRelated( url, term ) {
 		println url+term
 		def rest = new RestBuilder()
